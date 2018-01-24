b681fc014a360b14a8dd9aeea71f64cdd19d7ee3
bharathisubramani <31499305+bharathisubramani@users.noreply.github.com>
2018-01-24 23:21:10 +0530

    Create power of 2.c

---
+++ power of 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number which is power of 2");
+scanf("%d",&n);
+if(n==4)
+{
+printf("\nyes");
+}
+else
+{
+printf("\nno");
+}
+return 0;
+}
