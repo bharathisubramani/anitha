6a3d450b6de31441aad8ea6ef87faa7d95b5830b
bharathisubramani <31499305+bharathisubramani@users.noreply.github.com>
2018-02-28 15:54:08 +0530

    Update vowe.c

--- vowe.c
+++ vowe.c
@@ -3,12 +3,18 @@
 int main()
 {
 char a[100];
+int i,l,c=0;
 gets(a);
-l=strlen(a)
+l=strlen(a);
 for(i=0;i<l;i++)
 {
-if(((a[i]=='a')&&(a[i]=='e')&&(a[i]=='i')&&(a[i]=='o')&&(a[i]=='u'))||((a[i]=='A')&&(a[i]=='E')&&(a[i]=='I')&&(a[i]=='O')&&(a[i]=='U')))
+if(((a[i]=='a')||(a[i]=='e')||(a[i]=='i')||(a[i]=='o')||(a[i]=='u'))||((a[i]=='A')||(a[i]=='E')||(a[i]=='I')||(a[i]=='O')||(a[i]=='U')))
 {
+  c++;
+}
+}
+  if(c!=0)
+  {
 printf("yes");
 }
 else
