5d51128aa86c12fde4cfeb5ce5b0b538355de3d9
bharathisubramani <31499305+bharathisubramani@users.noreply.github.com>
2018-03-02 16:55:18 +0530

    Update san.c

--- san.c
+++ san.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main()
 {
-int i,l;
+int i,l,f=0;
 char a[100];
 gets(a);
 l=strlen(a);
@@ -10,6 +10,10 @@ for(i=0;i<l;i++)
 {
 if((a[i]>='0')&&(a[i]<='9')&&((a[i]>='a')&&(a[i]<='z')||(a[i]>='A')&&(a[i]<='Z')))
 {
+  f++;
+}
+  if(f!=0)
+  {
 printf("yes");
 }
 else
