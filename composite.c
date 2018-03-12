ef0b5f7e3c273729eaf227f6eb62ae4ede897abf
bharathisubramani <31499305+bharathisubramani@users.noreply.github.com>
2018-03-12 16:14:43 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,i,c=0;
+scanf("%d",&n);
+for(i=2;i<=a/2;i++)
+{
+if(a%i==0)
+{
+f=1;
+break;
+}
+}
+if(f==1)
+printf("composite");
+else
+printf("not composite");
+return 0;
+}
